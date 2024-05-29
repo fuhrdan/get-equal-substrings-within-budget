b4ba52ccfe5940c30b3c0c8c9ae25cc6e4b74682
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-05-29 11:32:24 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿int equalSubstring(char* s, char* t, int maxCost) {
+    int len = strlen(s);
+    int i = 0;
+    int j = 0;
+    int answer = 0;
+    int remainingCost = maxCost;
+    int currentCost = 0;
+    int con1 = 0;
+    int con2 = 0;
+    while (j < len)
+    {  
+        con1 = s[j];
+        con2 = t[j];
+        if (con1 - con2 > 0)
+        {
+            currentCost = con1 - con2;
+        }
+        else
+        {
+            currentCost = con2 - con1;
+        }
+        remainingCost = remainingCost - currentCost;
+        while (remainingCost < 0)
+        {
+            con1 = s[i];
+            con2 = t[i];
+            if (con1 - con2 > 0)
+            {
+                remainingCost = remainingCost + con1 - con2;
+            }
+            else
+            {
+                remainingCost = remainingCost + con2 - con1;
+            }
+            i++;
+        }
+        if(answer < (j - i + 1))
+        {
+            answer = j - i + 1;
+        }
+        j++;
+
+    }
+    return answer;
+}
